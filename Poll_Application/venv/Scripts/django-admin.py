5eb412e3bddc9a1bbd81c9cbffa3f3577b3366aa
John Farina <john.farina0824@gmail.com>
2019-02-20 01:12:27 -0500

    Initializing files for a poll application

---
+++ Poll_Application/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\johnf\Documents\Python\Django\Django Documentation\Poll_Application\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
